428178a8a8f574f65724e32e081109c1ec951df5
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-03-14 09:55:59 -0700

    Added methods to calculate median and quartiles

--- src/StatsCalculator.java
+++ src/StatsCalculator.java
@@ -38,6 +38,11 @@ public class StatsCalculator {
         Arrays.sort(sortedValues);
     }
 
+    /**
+     * Calculates the maximum value present in the array
+     *
+     * @return The maximum value
+     */
     public double calculateMax(){
         double max = values[0];
         for(double num : values){
@@ -46,5 +51,73 @@ public class StatsCalculator {
             }
         }
         return max;
+    }
+
+    /**
+     * Calculates the minimum value present in the array
+     *
+     * @return The minimum value
+     */
+    public double calculateMin(){
+        double min = values[0];
+        for(double num : values){
+            if(num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Calculates the median of the sorted array
+     *
+     * @return The median value
+     */
+    public double calculateMedian(){
+        double median = -1;
+        if(sortedValues.length % 2 == 0){
+            median = (sortedValues[sortedValues.length / 2] + sortedValues[sortedValues.length / 2 - 1]) / 2.0;
+        } else {
+            median = sortedValues[sortedValues.length/2];
+        }
+        return median;
+    }
+
+    /**
+     * Private method used to calculate the quartiles
+     *
+     * @param half An array consisting of the first or second half of the sorted array
+     * @return returns the median of 'half', which is the desired quartile
+     */
+    private double calculateQuartile(double[] half){
+        double quartile = -1;
+        if(half.length % 2 == 0){
+            quartile = (half[half.length / 2] + half[half.length / 2 - 1]) / 2.0;
+        } else {
+            quartile = half[half.length/2];
+        }
+        return quartile;
+    }
+
+    /**
+     * Calculates the first quartile of the sorted array
+     *
+     * @return The first quartile of the data set
+     */
+    public double calculateFirstQuartile(){
+        double[] firstHalf = new double[sortedValues.length / 2 - 1];
+            for(int i = 0; i < firstHalf.length; i++){
+                firstHalf[i] = sortedValues[i];
+            }
+            return calculateQuartile(firstHalf);
+    }
+
+
+    public double calculateThirdQuartile(){
+        double[] secondHalf = new double[sortedValues.length / 2 - 1];
+        for(int i = 0; i < secondHalf.length; i++){
+            secondHalf[i] = sortedValues[i];
+        }
+        return calculateQuartile(secondHalf);
     }
 }
